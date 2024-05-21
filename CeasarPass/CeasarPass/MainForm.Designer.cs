6e69a288a3d6a859ac14d3152761185d795e7b0d
kaur41 <m1kchan@yandex.ru>
2024-05-22 01:55:10 +0300

    This program implements the Caesar cipher, reads and writes to a file, and fixes all bugs.

---
+++ CeasarPass/CeasarPass/MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace CeasarPass
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Коэфиценты = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            optionsToolStripMenuItem = new ToolStripMenuItem();
+            backgroundColorToolStripMenuItem = new ToolStripMenuItem();
+            buttonStyleToolStripMenuItem = new ToolStripMenuItem();
+            comboBox1 = new ComboBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Коэфиценты
+            // 
+            Коэфиценты.AutoSize = true;
+            Коэфиценты.Location = new Point(12, 34);
+            Коэфиценты.Name = "Коэфиценты";
+            Коэфиценты.Size = new Size(42, 15);
+            Коэфиценты.TabIndex = 0;
+            Коэфиценты.Text = "Action";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(26, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Key";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(261, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Text";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(254, 71);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(277, 120);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, optionsToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(543, 24);
+            menuStrip1.TabIndex = 4;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            fileToolStripMenuItem.Click += fileToolStripMenuItem_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(180, 22);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(180, 22);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { backgroundColorToolStripMenuItem, buttonStyleToolStripMenuItem });
+            optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            optionsToolStripMenuItem.Size = new Size(61, 20);
+            optionsToolStripMenuItem.Text = "Options";
+            optionsToolStripMenuItem.Click += optionsToolStripMenuItem_Click;
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            backgroundColorToolStripMenuItem.Size = new Size(170, 22);
+            backgroundColorToolStripMenuItem.Text = "Background Color";
+            // 
+            // buttonStyleToolStripMenuItem
+            // 
+            buttonStyleToolStripMenuItem.Name = "buttonStyleToolStripMenuItem";
+            buttonStyleToolStripMenuItem.Size = new Size(170, 22);
+            buttonStyleToolStripMenuItem.Text = "Button Style";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Decrypt", "Ecrypt" });
+            comboBox1.Location = new Point(15, 52);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 5;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(15, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(56, 23);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 168);
+            button1.Name = "button1";
+            button1.Size = new Size(146, 23);
+            button1.TabIndex = 7;
+            button1.Text = "Do it!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(543, 203);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Коэфиценты);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            Text = "MainForm";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Коэфиценты;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem optionsToolStripMenuItem;
+        private ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private ToolStripMenuItem buttonStyleToolStripMenuItem;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ CeasarPass/CeasarPass/MainForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CeasarPass
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            /* string m = textBox1.Text;
+             int nomer; // Номер в алфавите
+
+             string s; //Результат
+             int j; // Переменная для циклов
+
+
+             char[] massage = m.ToCharArray(); // Превращаем строку в массив символов.
+             char[] alfavit = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+             for (int i = 0; i < massage.Length; i++)
+             {
+                 // Ищем индекс буквы
+                 for (j = 0; j < alfavit.Length; j++)
+                 {
+                     if (massage[i] == alfavit[j])
+                     {
+                         break;
+                     }
+                 }
+                 if (j != 33) // Если j равно 33, значит символ не из алфавита
+                 {
+                     nomer = j; // Индекс буквы
+                     d = nomer + 3; // Делаем смещение
+
+                     // Проверяем, чтобы не вышли за пределы алфавита
+                     if (d > 32)
+                     {
+                         d = d - 33;
+                     }
+
+                     massage[i] = alfavit[d]; // Меняем букву
+                 }
+             }
+             s = new string(massage); // Собираем символы обратно в строку.
+             File.WriteAllText("2.txt", s); // Записываем результат в файл.*/
+
+        }
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string config = comboBox1.Text;
+            string m = textBox1.Text;
+            int nomer; // Номер в алфавите
+            int key = 0;
+            string s; //Результат
+            int j; // Переменная для циклов
+                   //string user_key = textBox2.Text; // смещение 
+            bool isNumeric = int.TryParse(textBox2.Text, out key);
+            string result;
+            char[] massage = m.ToCharArray(); // Превращаем строку в массив символов.
+            char[] alfavit = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'Z', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X' };
+            if (!isNumeric)
+            {
+                MessageBox.Show("Please enter a valid integer key.");
+                return;
+            }
+            if (config == "Encrypt")
+            {
+                for (int i = 0; i < massage.Length; i++)
+                {
+                    // Ищем индекс буквы
+                    for (j = 0; j < alfavit.Length; j++)
+                    {
+                        if (massage[i] == alfavit[j])
+                        {
+                            massage[i] = alfavit[j + key % alfavit.Length];
+                            break;
+                        }
+
+                    }
+                    // if (j == massage.Length) { break; }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < massage.Length; i++)
+                {
+                    // Ищем индекс буквы
+                    for (j = 0; j < alfavit.Length; j++)
+                    {
+                        if (massage[i] == alfavit[j])
+                        {
+                            massage[i] = alfavit[j - key % alfavit.Length];
+                            break;
+                        }
+
+                    }
+                }
+            }
+            result = new string(massage);
+            MessageBox.Show(result);
+        }
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        string fileContent = sr.ReadToEnd();
+                        //textBox1.Text("Содержимое файла:\n\n" + fileContent, "Чтение файла", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        textBox1.Text = fileContent;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при чтении файла: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Выберите место для сохранения файла";
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(filePath))
+                    {
+                        sw.Write(textBox1.Text); // Записываем текст из TextBox в файл
+                        MessageBox.Show("Текст успешно записан в файл.", "Запись в файл", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при записи в файл: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
